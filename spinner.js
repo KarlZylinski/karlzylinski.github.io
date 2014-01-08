9e5281bcc8a351e866ebe94798e49539d7b8b276
Karl Zylinski <karl@zylinski.se>
2014-01-08 22:13:26 +0100

    Spinners with descriptions

--- spinner.js
+++ spinner.js
@@ -1,16 +1,8 @@
-var current_image = 0;
-var num_images = 0;
-var spinner_id = 0;
-var aborted = false;
 var hovering = false;
 
-function show_next_spinner_image(sid, container) {
+function show_next_spinner_image(num_images, current_image, container) {
 	if(hovering) {
-		setTimeout("show_next_spinner_image(" + sid + ", \"" + container + "\")", 5000);
-		return;
-	}
-
-	if(sid != spinner_id) {
+		setTimeout("show_next_spinner_image(" + num_images + ", " + current_image + ", \""  + container + "\")", 5000);
 		return;
 	}
 	
@@ -19,12 +11,12 @@ function show_next_spinner_image(sid, container) {
 	if(current_image < 0)
 		current_image = num_images - 1;
 	
-	var spinner_image = $(container + ' img').eq(current_image);
+	var spinner_image = $(container + ' div').eq(current_image);
 	
-	$(container + ' img').hide();
+	$(container + ' div').hide();
 	spinner_image.show();
 	
-	setTimeout("show_next_spinner_image(" + sid + ", \""  + container + "\")", 5000);
+	setTimeout("show_next_spinner_image(" + num_images + ", " + current_image + ", \""  + container + "\")", 5000);
 }
 
 function start_spinner(container) {
@@ -36,8 +28,12 @@ function start_spinner(container) {
 		hovering = false;
 	});
 
-	var spinner_images = $(container + ' img');
-	num_images = spinner_images.size();
+	var spinner_images = $(container + ' div');
+	var num_images = spinner_images.size();
+
+	var spinner_image = $(container + ' div').eq(0);
+	$(container + ' div').hide();
+	spinner_image.show();
 	
-	show_next_spinner_image(0, container);
+	show_next_spinner_image(num_images, 0, container);
 }
