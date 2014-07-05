6663018e857a910ffdb2db13b5eba80d1610935d
Karl Zylinski <karl@zylinski.se>
2014-07-05 02:01:29 +0200

    Moved image text out from image and changed it's style.

--- spinner.js
+++ spinner.js
@@ -11,9 +11,9 @@ function show_next_spinner_image(num_images, current_image, container) {
 	if(current_image < 0)
 		current_image = num_images - 1;
 	
-	var spinner_image = $(container + ' div').eq(current_image);
+	var spinner_image = $(container + ' .item').eq(current_image);
 	
-	$(container + ' div').hide();
+	$(container + ' .item').hide();
 	spinner_image.show();
 	
 	setTimeout("show_next_spinner_image(" + num_images + ", " + current_image + ", \""  + container + "\")", 5000);
@@ -28,11 +28,11 @@ function start_spinner(container) {
 		hovering = false;
 	});
 
-	var spinner_images = $(container + ' div');
+	var spinner_images = $(container + ' .item');
 	var num_images = spinner_images.size();
 
-	var spinner_image = $(container + ' div').eq(0);
-	$(container + ' div').hide();
+	var spinner_image = $(container + ' .item').eq(0);
+	$(container + ' .item').hide();
 	spinner_image.show();
 	
 	show_next_spinner_image(num_images, 0, container);
