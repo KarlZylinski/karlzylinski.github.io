50349a30e279c50d15897a282946772da0cb6592
Karl Zylinski <karl@zylinski.se>
2015-03-17 23:13:33 +0100

    Updated bezier fun.

--- bezier_fun/bezier-fun.js
+++ bezier_fun/bezier-fun.js
@@ -4,9 +4,7 @@ window.onload = function() {
     var canvasHeight = canvas.height
     var context = canvas.getContext("2d")
     var canvasData = context.getImageData(0, 0, canvasWidth, canvasHeight)
-    var dragging = false
 
-    // That's how you define the value of a pixel //
     function drawPixel (x, y, r, g, b, a) {
         if (x < 0 || x >= canvasWidth || y < 0 || y >= canvasHeight)
             return
@@ -35,24 +33,38 @@ window.onload = function() {
         [220, 40]
     ]
 
+    var draggedControlPoint = null;
+
     canvas.addEventListener("mousedown", function(evt) {
         var x = evt.clientX
         var y = evt.clientY
+
+        if (evt.button == 1)
+        {
+            controlPoints.push([x, y])
+            render()
+            return
+        }
 
         for (var i = 0; i < controlPoints.length; ++i) {
             var cp = controlPoints[i]
 
             if (x > cp[0] - 5 && x < cp[0] + 5
                 && y > cp[1] - 5 && y < cp[1] + 5) {
+                draggedControlPoint = cp;
+
                 function updateControlPoint(evt) {
                     controlPoints[i][0] = Math.max(Math.min(evt.clientX, canvasWidth), 0)
                     controlPoints[i][1] = Math.max(Math.min(evt.clientY, canvasHeight), 0)
+                    render()
                 }
 
                 document.addEventListener("mousemove", updateControlPoint)
 
                 document.addEventListener("mouseup", function(evt) {
+                    draggedControlPoint = null;
                     document.removeEventListener("mousemove", updateControlPoint);
+                    render()
                 })
 
                 return
@@ -60,34 +72,45 @@ window.onload = function() {
         }
     }, false)
 
+    function calculateBezierPoint(t, p0, p1, p2, p3) {
+        var tInv = 1.0 - t
+        var tInv2 = tInv * tInv
+        var tInv3 = tInv * tInv * tInv
+        var t2 = t * t
+        var t3 = t * t * t
+        var x = p0[0] * tInv3 + p1[0] * 3 * tInv2 * t + p2[0] * 3 * tInv * t2 + p3[0] * t3
+        var y = p0[1] * tInv3 + p1[1] * 3 * tInv2 * t + p2[1] * 3 * tInv * t2 + p3[1] * t3
+        return [x, y]
+    }
+
     function render() {
         clearCanvas()
 
-        var t = 0.0;
-        while (t <= 1.0) {
-            var tInv = 1.0 - t
-            var tInv2 = tInv * tInv
-            var tInv3 = tInv * tInv * tInv
-            var t2 = t * t
-            var t3 = t * t * t
+        for (var i = 0; i < controlPoints.length - 3; i += 3) {
+            var t = 0.0
 
-            var x = controlPoints[0][0] * tInv3 + controlPoints[1][0] * 3 * tInv2 * t + controlPoints[2][0] * 3 * tInv * t2 + controlPoints[3][0] * t3
-            var y = controlPoints[0][1] * tInv3 + controlPoints[1][1] * 3 * tInv2 * t + controlPoints[2][1] * 3 * tInv * t2 + controlPoints[3][1] * t3
-            drawPixel(x, y, 200, 200, 255, 255)
-            t += 1.0/canvasWidth
+            while (t <= 1.0) {
+                var point = calculateBezierPoint(t, controlPoints[i], controlPoints[i + 1], controlPoints[i + 2], controlPoints[i + 3])
+                drawPixel(point[0], point[1], 200, 200, 255, 255)
+                t += 1.0/canvasWidth
+            }
         }
 
         for (var i = 0; i < controlPoints.length; ++i) {
             var cp = controlPoints[i]
             for (var x = cp[0] - 3; x < cp[0] + 3; ++x) {
                 for (var y = cp[1] - 3; y < cp[1] + 3; ++y)
-                    drawPixel(x, y, 255, 200, 0, 255)
+                {
+                    if (draggedControlPoint != null && draggedControlPoint == cp)
+                        drawPixel(x, y, 200, 0, 255, 255)
+                    else
+                        drawPixel(x, y, 255, 200, 0, 255)
+                }
             }
         }
 
         updateCanvas()
-        requestAnimationFrame(render)
     }
-
-    requestAnimationFrame(render)
+    
+    render()
 }
