d5528b6c5f5956ebcc375a673204f56cc8d894c6
Karl Zylinski <karl@zylinski.se>
2015-03-22 01:22:11 +0100

    Updated bezier fun.

--- bezier_fun/bezier-fun.js
+++ bezier_fun/bezier-fun.js
@@ -26,14 +26,88 @@ window.onload = function() {
         context.putImageData(canvasData, 0, 0)
     }
 
-    var controlPoints = [
-        [120, 160],
-        [35, 200],
-        [220, 260],
-        [220, 40]
-    ]
+    function createCurve(p1, p2, controlPoint1, controlPoint2) {
+        return {
+            point1: p1,
+            point2: p2,
+            controlPoint1: controlPoint1,
+            controlPoint2: controlPoint2
+        }
+    }
 
-    var draggedControlPoint = null;
+    var curves = []
+    curves.push(createCurve([120, 160], [220, 40], [35, 200], [220, 260]))
+    var draggedControlPoint = null
+
+    var PointType = {
+        Point1: 0,
+        Point2: 1,
+        ControlPoint1: 2,
+        ControlPoint2: 3
+    }
+
+    function getPointFromType(curve, type) {
+        if (type == PointType.Point1)
+            return curve.point1
+
+        if (type == PointType.Point2)
+            return curve.point2
+
+        if (type == PointType.ControlPoint1)
+            return curve.controlPoint1
+
+        if (type == PointType.ControlPoint2)
+            return curve.controlPoint2
+    }
+
+    function tryDragPoint(mouseX, mouseY, curve, pointType) {
+        var point = getPointFromType(curve, pointType)
+
+        if (mouseX < point[0] - 5 || mouseX > point[0] + 5 || mouseY < point[1] - 5 || mouseY > point[1] + 5)
+            return false
+
+        draggedControlPoint = point;
+
+        function updateControlPoint(evt) {
+            var newX = Math.max(Math.min(evt.clientX, canvasWidth), 0)
+            var newY = Math.max(Math.min(evt.clientY, canvasHeight), 0)
+
+            var deltaX = newX - point[0]
+            var deltaY = newY - point[1]
+            point[0] = newX
+            point[1] = newY
+            var middleX  = (curve.point1[0] + curve.point2[0])/2
+            var middleY  = (curve.point1[1] + curve.point2[1])/2
+
+            if (pointType == PointType.ControlPoint1) {
+                curve.controlPoint2[0] = middleX - (newX - middleX)
+                curve.controlPoint2[1] = middleY - (newY - middleY)
+            }
+            else if (pointType == PointType.ControlPoint2) {
+                curve.controlPoint1[0] = middleX - (newX - middleX)
+                curve.controlPoint1[1] = middleY - (newY - middleY)
+            }
+            else {
+                // Moved endpoints, update control points
+                curve.controlPoint1[0] += deltaX
+                curve.controlPoint1[1] += deltaY
+                curve.controlPoint2[0] += deltaX
+                curve.controlPoint2[1] += deltaY
+            }
+
+            render()
+        }
+
+        document.addEventListener("mousemove", updateControlPoint)
+
+        document.addEventListener("mouseup", function(evt) {
+            draggedControlPoint = null;
+            document.removeEventListener("mousemove", updateControlPoint);
+            render()
+        })
+
+        return true
+    }
 
     canvas.addEventListener("mousedown", function(evt) {
         var x = evt.clientX
@@ -41,72 +115,69 @@ window.onload = function() {
 
         if (evt.button == 1)
         {
-            controlPoints.push([x, y])
+            var p1 = curves[curves.length - 1].point2
+            var p2 = [x, y]
+            curves.push(createCurve(p1, p2, [p1[0] + 10, p1[1] + 10], [x + 10, y + 10]))
             render()
             return
         }
 
-        for (var i = 0; i < controlPoints.length; ++i) {
-            var cp = controlPoints[i]
+        for (var i = 0; i < curves.length; ++i) {
+            var curve = curves[i]
 
-            if (x > cp[0] - 5 && x < cp[0] + 5
-                && y > cp[1] - 5 && y < cp[1] + 5) {
-                draggedControlPoint = cp;
-
-                function updateControlPoint(evt) {
-                    controlPoints[i][0] = Math.max(Math.min(evt.clientX, canvasWidth), 0)
-                    controlPoints[i][1] = Math.max(Math.min(evt.clientY, canvasHeight), 0)
-                    render()
-                }
-
-                document.addEventListener("mousemove", updateControlPoint)
-
-                document.addEventListener("mouseup", function(evt) {
-                    draggedControlPoint = null;
-                    document.removeEventListener("mousemove", updateControlPoint);
-                    render()
-                })
-
+            if (tryDragPoint(x, y, curve, PointType.ControlPoint1))
                 return
-            }
+
+            if (tryDragPoint(x, y, curve, PointType.ControlPoint2))
+                return
+
+            if (tryDragPoint(x, y, curve, PointType.Point1))
+                return
+
+            if (tryDragPoint(x, y, curve, PointType.Point2))
+                return
         }
     }, false)
 
-    function calculateBezierPoint(t, p0, p1, p2, p3) {
+    function calculateBezierPoint(t, point1, point2, controlPoint1, controlPoint2) {
         var tInv = 1.0 - t
         var tInv2 = tInv * tInv
         var tInv3 = tInv * tInv * tInv
         var t2 = t * t
         var t3 = t * t * t
-        var x = p0[0] * tInv3 + p1[0] * 3 * tInv2 * t + p2[0] * 3 * tInv * t2 + p3[0] * t3
-        var y = p0[1] * tInv3 + p1[1] * 3 * tInv2 * t + p2[1] * 3 * tInv * t2 + p3[1] * t3
+        var x = point1[0] * tInv3 + controlPoint1[0] * 3 * tInv2 * t + controlPoint2[0] * 3 * tInv * t2 + point2[0] * t3
+        var y = point1[1] * tInv3 + controlPoint1[1] * 3 * tInv2 * t + controlPoint2[1] * 3 * tInv * t2 + point2[1] * t3
         return [x, y]
+    }
+
+    function drawHandle(point) {
+        for (var x = point[0] - 3; x < point[0] + 3; ++x) {
+            for (var y = point[1] - 3; y < point[1] + 3; ++y) {
+                if (draggedControlPoint != null && draggedControlPoint == point)
+                    drawPixel(x, y, 200, 0, 255, 255)
+                else
+                    drawPixel(x, y, 255, 200, 0, 255)
+            }
+        }
     }
 
     function render() {
         clearCanvas()
 
-        for (var i = 0; i < controlPoints.length - 3; i += 3) {
+        for (var i = 0; i < curves.length; ++i) {
             var t = 0.0
+            var curve = curves[i]
 
             while (t <= 1.0) {
-                var point = calculateBezierPoint(t, controlPoints[i], controlPoints[i + 1], controlPoints[i + 2], controlPoints[i + 3])
+                var point = calculateBezierPoint(t, curve.point1, curve.point2, curve.controlPoint1, curve.controlPoint2)
                 drawPixel(point[0], point[1], 200, 200, 255, 255)
                 t += 1.0/canvasWidth
             }
-        }
 
-        for (var i = 0; i < controlPoints.length; ++i) {
-            var cp = controlPoints[i]
-            for (var x = cp[0] - 3; x < cp[0] + 3; ++x) {
-                for (var y = cp[1] - 3; y < cp[1] + 3; ++y)
-                {
-                    if (draggedControlPoint != null && draggedControlPoint == cp)
-                        drawPixel(x, y, 200, 0, 255, 255)
-                    else
-                        drawPixel(x, y, 255, 200, 0, 255)
-                }
-            }
+            drawHandle(curve.point1)
+            drawHandle(curve.point2)
+            drawHandle(curve.controlPoint1)
+            drawHandle(curve.controlPoint2)
         }
 
         updateCanvas()
