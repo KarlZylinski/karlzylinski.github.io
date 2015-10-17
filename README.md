71a1c6909dbcf02f892bcb3ae234602b23a9d441
Karl Zylinski <karl@zylinski.se>
2015-10-18 00:35:06 +0200

    New site in the works.

--- README.md
+++
@@ -1,4 +0,0 @@
-karlzylinski.github.io
-======================
-
-My personal website
---
+++ build.py
@@ -0,0 +1,15 @@
+content_files = ["index", "games"]#, "music", "drawings", "photos"]
+
+with open('template.html', 'r') as template_file:
+    template = template_file.read()
+    content_marker ="%%%"
+    content_index = template.find(content_marker)
+    content_header = template[0:content_index]
+    content_footer = template[content_index + len(content_marker):len(template)]
+    for name in content_files:
+        with open(name + "_content.html", 'r') as content_file:
+            content = content_file.read()
+            with open(name + ".html", 'w') as page_file:
+                page_file.write(content_header)
+                page_file.write(content)
+                page_file.write(content_footer)
